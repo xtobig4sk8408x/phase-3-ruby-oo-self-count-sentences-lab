2db16dde333bd8145ce88ce9209abb96a2140bc9
Theodore John Josef <john.josef@aol.com>
2023-01-11 21:57:03 -0800

    commit tree 049c455da009f312f7a7095422ee1facb24ebbe6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_count = self.split(/[.!?]+[^.!?]/).length
+    sentence_count 
   end
 end
